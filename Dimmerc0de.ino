dac99fe70ad9f8aa9f28db0940d93f553539a95f
Kunsu Hammond <officiallababy01@gmail.com>
2025-07-17 19:47:18 +0000

    Add files via upload

---
+++ Dimmerc0de.ino
@@ -0,0 +1,16 @@
+int potPin = 15;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int potValue = analogRead(potPin);
+
+Serial.print("Potentiometer  Value: ");
+Serial.println(potValue);
+
+delay(1000);
+}
